d681fda97d5120aa61f01b735d0f56b197008607
Caio Diego <caiodsj96@gmail.com>
2023-10-23 20:19:46 -0300

    feat: userModel

---
+++ ID_model/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID_model.Models
+{
+    internal class UserModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
